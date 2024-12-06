4ee924ee38932fae62ef9eec509febbfca361ac5
janu47537 <janu47537@gmail.com>
2024-12-06 11:26:32 +0530

    Add files via upload

---
+++ Christmasgreetings.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Christmasgreetings
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int d=sc.nextInt();
+	    if(d==25)
+	    {
+	        System.out.println("christmas");
+	    }
+	    else
+	    {
+	        System.out.println("Ordinary");
+	    }
+	 
+	}
+}
