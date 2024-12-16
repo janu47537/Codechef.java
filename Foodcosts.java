3ac6c843d76f957ac38c05a227e447ffef8b2145
A Janani Priya <janu47537@gmail.com>
2024-12-16 10:57:59 +0530

    Add files via upload

---
+++ Foodcosts.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Foodcosts
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int Weeklycost=(6*X)+(1*Y);
+		System.out.println(Weeklycost);
+
+	}
+}
+	
