7d0f5b2919209edf4c735c69985ca6b9fca56863
A Janani Priya <janu47537@gmail.com>
2024-12-16 11:59:12 +0530

    Add files via upload

--- CodechefRound.java
+++ CodechefRound.java
@@ -1,14 +1,18 @@
 import java.util.*;
-class CodechefRound
+class Codechef
 {
 	public static void main (String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		int N=sc.nextInt();
-		if(N>=1 && N<=7)
+	    int x=sc.nextInt();
+		if(x==4)
 		{
-		 System.out.println((N==4)?"YES":"N0");
+		 System.out.println("YES");
+		 }
+		 else{
+		     System.out.println("NO");
 		 }
 
 	}
 }
+
