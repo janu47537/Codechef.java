5cb378e72f8835643d662ffc7408bcb608fbe4d1
A Janani Priya <janu47537@gmail.com>
2025-01-16 20:39:43 +0530

    Add files via upload

---
+++ CHEFSOCKS.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class CHEFSOCKS
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int A=sc.nextInt();
+	 int X=sc.nextInt();
+	 int Y=sc.nextInt();
+	 if(A<=X+Y)
+	  System.out.println("YES");
+	  else
+	  System.out.println("NO");
+    }
+}
