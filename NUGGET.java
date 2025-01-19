e5d4a4807b9390ceef42e6ad3d7e538b4ac01f51
A Janani Priya <janu47537@gmail.com>
2025-01-19 17:48:25 +0530

    Add files via upload

---
+++ NUGGET.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class NUGGET
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    System.out.println(X*5000+Y*9800);
+		
+
+	}
+}
