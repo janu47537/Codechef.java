3030b2137babe283b3856923814cd9e5b1d03de7
A Janani Priya <janu47537@gmail.com>
2025-01-19 21:43:53 +0530

    Add files via upload

---
+++ ACPLEASE.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ACPLEASE
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int T=sc.nextInt();
+	    if(T>30)
+	    {
+	        System.out.println("YES");
+	    }
+	    else{
+	        System.out.println("NO");
+	    }
+	
+
+	}
+}
