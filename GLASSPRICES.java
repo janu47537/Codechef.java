a7d90700e4f6d33dba1361647f4c93bd9a9ac139
A Janani Priya <janu47537@gmail.com>
2025-01-19 17:41:45 +0530

    Add files via upload

---
+++ GLASSPRICES.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GLASSPRICES
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    if(X+X<Y)
+	    {
+	        System.out.println("PLASTIC");
+	    }
+	    else
+	    {
+	        System.out.println("METAL");
+	    }
+
+	}
+}
