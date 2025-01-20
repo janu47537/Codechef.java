4f65ef704b4ad6c74c11022594057a283887e17a
A Janani Priya <janu47537@gmail.com>
2025-01-20 16:18:15 +0530

    Add files via upload

---
+++ PUZZLEHUNT.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PUZZLEHUNT
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int N=sc.nextInt();
+	    if(N>=6 && N<=8)
+	     System.out.println("yes");
+	    else
+	    System.out.println("No");
+
+
+		
+
+	}
+}
