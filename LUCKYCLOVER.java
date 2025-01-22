de9234b1794f068c63d56b59018152550420248a
A Janani Priya <janu47537@gmail.com>
2025-01-22 21:16:49 +0530

    Add files via upload

---
+++ LUCKYCLOVER.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LUCKYCLOVER
+{
+	public static void main (String[] args) throws java.lang.Exception{
+	Scanner sc=new Scanner(System.in);
+	int N=sc.nextInt();
+	if(N==1)
+	System.out.println("4");
+	else{
+	int X=4+(3*(N-1));
+	System.out.println(X);
+	}
+	
+
+	}
+}
