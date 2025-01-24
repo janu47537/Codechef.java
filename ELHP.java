a313bdd6bbb13a6b938b8dc5c9d21a775290d41c
A Janani Priya <janu47537@gmail.com>
2025-01-24 21:29:11 +0530

    Add files via upload

---
+++ ELHP.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ELHP
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    if(X >= 2*Y)
+	    {
+	        System.out.println("Yes");
+	    }
+	    else
+	    {
+	        System.out.println("No");
+	    }
+	    
+
+	}
+}
