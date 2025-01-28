b209ed7ea0690b3cc67162125234f681a5dacf06
A Janani Priya <janu47537@gmail.com>
2025-01-28 21:25:55 +0530

    Add files via upload

---
+++ NEWSPAPER.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class NEWSPAPER
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    if(X<=7)
+	    {
+	        System.out.println("NO");
+	    }
+	    else
+	    {
+	        System.out.println("YES");
+	    }
+		
+
+	}
+}
