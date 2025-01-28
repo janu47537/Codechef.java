22341ba0c64aa33d9600e7077e730599a672fed6
A Janani Priya <janu47537@gmail.com>
2025-01-28 21:38:16 +0530

    Add files via upload

---
+++ WRITINGSPEED.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class WRITINGSPEED
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int totaltime=X*5;
+	    if(totaltime>60)
+	    {
+	        System.out.println("NO");
+	    }
+	    else
+	    {
+	        System.out.println("YES");
+	    }
+	
+
+	}
+}
