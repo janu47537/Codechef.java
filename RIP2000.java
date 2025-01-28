52f976fbf0ba066ebd57b96bd0ed96bffca426fe
A Janani Priya <janu47537@gmail.com>
2025-01-28 21:28:10 +0530

    Add files via upload

---
+++ RIP2000.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class RIP2000
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int N=sc.nextInt();
+	    int totalnotes=N*2000;
+	    System.out.println(totalnotes/500);
+	  
+
+	}
+}
