539d2a5afae40edb30f336854cef2e1e04438b28
A Janani Priya <janu47537@gmail.com>
2025-02-14 10:29:11 +0530

    Add files via upload

---
+++ Octobermarathon.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Octobermarathon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    if(X<3)
+	    System.out.println("GOLD");
+	    else if(X>=3 && X<6)
+	    System.out.println("SILVER");
+	    else if(X>=6)
+	    System.out.println("BRONZE");
+	
+
+	}
+}
