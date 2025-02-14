1089970ef2e94e52679510d859b74622a1659230
A Janani Priya <janu47537@gmail.com>
2025-02-14 10:31:34 +0530

    Add files via upload

---
+++ Valentinescontest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Valentinescontest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int N=sc.nextInt();
+	    if(N==121)
+	    {
+	     System.out.println("Likely");   
+	    }
+	    else{
+	        System.out.println("Unlikely");
+	    }
+	
+
+	}
+}
