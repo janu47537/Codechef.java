63958db8086dfec2c20809dfbe31c8b82b5c7e9b
A Janani Priya <janu47537@gmail.com>
2025-02-14 10:46:51 +0530

    Add files via upload

---
+++ Boneapetit.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Boneapetit
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scn = new Scanner(System.in);
+		int n  = scn.nextInt();
+		int m = scn.nextInt();
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		
+		System.out.println((n*x)+(m*y));
+	}
+}
