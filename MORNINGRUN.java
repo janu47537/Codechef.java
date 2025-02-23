ca8275ab7bde81d70dfd975c55f9280531685bd3
A Janani Priya <janu47537@gmail.com>
2025-02-23 18:37:43 +0530

    Add files via upload

---
+++ MORNINGRUN.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class MORNINGRUN
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    if(X+Y+X+Y>=1000)
+	    System.out.println("YES");
+	    else
+	    System.out.println("NO");
+		
+
+	}
+}
