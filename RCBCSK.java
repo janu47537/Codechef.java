63afe479af4dd0026ae1ad3bc48f28a2554d53c2
A Janani Priya <janu47537@gmail.com>
2025-02-27 21:10:56 +0530

    Add files via upload

---
+++ RCBCSK.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class RCBCSK
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int b = sc.nextInt();
+		if(t-b>=18) System.out.println("RCB");
+		else 
+		System.out.println("CSK");
+
+	}
+}
