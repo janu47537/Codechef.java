b5b1e59ed341833232dad09d6658ba8b5e7ec25d
A Janani Priya <janu47537@gmail.com>
2025-02-28 11:22:48 +0530

    Add files via upload

---
+++ Toomuchhomework.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Toomuchhomework
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    if((X+(10*Y))>=100)
+	    System.out.println("YES");
+	    else
+	    System.out.println("NO");
+	
+
+	}
+}
