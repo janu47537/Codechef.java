0a8fee0569a44130fc5cf9cec6d03b60066fad8a
A Janani Priya <janu47537@gmail.com>
2025-02-28 10:09:29 +0530

    Add files via upload

---
+++ Olympics2024.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Olympics2024
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int G=sc.nextInt();
+	    int S=sc.nextInt();
+	    int B=sc.nextInt();
+	    int total=(G+S+B);
+	    System.out.println((15-(total)));
+		
+
+	}
+}
